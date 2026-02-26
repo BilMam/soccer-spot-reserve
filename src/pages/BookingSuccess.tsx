a7383ca24dfa712e08e578dd0051f8fee1cf7054
PISport AutoSync <auto@pisport.app>
2026-02-26 01:49:23 +0000

    auto-sync: 26/02 01:49

--- src/pages/BookingSuccess.tsx
+++ src/pages/BookingSuccess.tsx
@@ -167,7 +167,7 @@ const BookingSuccess = () => {
 
               <div className="flex items-center space-x-3">
                 <Clock className="w-5 h-5 text-gray-500" />
-                <span>{booking.start_time} - {booking.end_time}</span>
+                <span>{formatTime(booking.start_time)} - {formatTime(booking.end_time)}</span>
               </div>
 
               <div className="border-t pt-4 space-y-2">
